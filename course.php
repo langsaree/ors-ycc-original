171f5fca840153d8d16630bd9ac044da13702ac0
Haris <Harisyoonuh14@gmail.com>
2019-08-06 15:14:04 +0700

    Update course.php

--- course.php
+++ course.php
@@ -1,43 +1,8 @@
 <?php
 session_start();
-if(!session_is_registered(username))
+if(!isset($_SESSION["username"]))
 	{
-     include('db.php');
-     $username = "";
-     $password = "";
-     if(!isset($_SESSION['logined'])) {
-      if(isset($_REQUEST['username'])) {
-        $username = $_REQUEST['username'];
-        $password = $_REQUEST['password'];
-			if(empty($_REQUEST['username']) && empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกชื่อผู้ใช้และรหัสผ่านของท่านด้วย</span>';
-			} else if(empty($_REQUEST['username']) && !empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกชื่อผู้ใช้ของท่านด้วย</span>';
-			} else if(!empty($_REQUEST['username']) && empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกรหัสผ่านของท่านด้วย</span>';
-			} else {
-			       $sql = "select * from student where username='$username' and password='$password'";
-                   $result=mysql_query($sql);
-                   $count=mysql_num_rows($result);
-                  if($count==1)
-                      {
-					  //$_SESSION['logined'] = true;
-					  //$_SESSION['username'] = $_REQUEST['username'];
-					  //$_SESSION['password'] = $_REQUEST['password'];
-					  session_register("username");
-                      session_register("password");
-					  //$_SESSION['username'] = $value["username"];
-                      //$_SESSION['password'] = $value["password"];
-					  header("location:std_profile.php");
-					  }
-				   else
-				   {
-				    $message = '<span style="color:red">ข้อมูลของท่านไม่ถูกต้อง กรุณาตรวจสอบข้อมูลด้วย</span>';
-				   }
-				  }
-			  	}  
-   
-}
+    include('login_check.php');
 ?>
 
 <!DOCTYPE html>
@@ -48,7 +13,7 @@ if(!session_is_registered(username))
     <title>หลักสูตรที่เปิดสอน</title>
     <link rel="stylesheet" href="style.css" />
     <style type="text/css">
-<!--
+
 .style25 {font-size: 11px; font-family: Tahoma; }
 .style9 {font-size: 12px}
 .style7 {color: #3987FB; font-size: 14px; }
@@ -57,7 +22,7 @@ if(!session_is_registered(username))
 	font-weight: bold;
 }
 .style28 {font-size: 12px; font-weight: bold; }
--->
+
     </style>
 </head>
 <body>
@@ -95,8 +60,12 @@ if(!session_is_registered(username))
 
 
 
-	<?php echo $message; ?>		
-	<? echo	'<form action="" method="post">
+              <?php
+              if (isset($message)) {
+                  echo $message;
+              }
+              ?>
+	<?php echo	'<form action="" method="post">
 		<table width="150" border="0" align="left" cellpadding="0" cellspacing="0">
               <tr>
                 <td></td>
