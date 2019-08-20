a0aeeeb3f55995a61994f932cd5aeaa8c487d2a0
Raiyan Chayen <raiyan.chayen@gmail.com>
2019-08-20 16:59:49 +0700

    create include/login.php

--- include/login.php
+++ include/login.php
@@ -1,78 +1,101 @@
 <?php
-if(!isset($_SESSION['username'])) // To check login user if already login then hide login form
-{
 
+include('db.php');
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+}
+// username and password receive from register form
+if (isset($_POST['ok'])) {
+    if (!empty($_POST['username'] && !empty($_POST['password']))) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        // To protect MySQL injection (more detail about MySQL injection)
+        $username = stripslashes($username);
+        $password = stripslashes($password);
+        $username = mysqli_real_escape_string($connection, $username);
+        $password = mysqli_real_escape_string($connection, $password);
+        //check compare to database
+        $sql = "SELECT * FROM student WHERE username='$username' and password='$password'";
+        $result = mysqli_query($connection, $sql);
+        // Mysql_num_row is counting table row
+        $count = mysqli_num_rows($result);
+        // If result matched $myusername and $mypassword, table row must be 1 row
+        if ($count == 1) {
+            // Register $username, $password and redirect to file "login_success.php"
+            $_SESSION['username'] = $username;
+//            $_SESSION['password'] = $password;
+            // redirect to profile page
+            header("Location:std_profile.php");
+        } else {
+            //$error='Wrong Username or Password';
+            $message = "ข้อมูลของท่านไม่ถูกต้อง กรุณาตรวจสอบข้อมูลด้วย";
+
+        }
+    } else if (empty($_POST['username']) && empty($_POST['password'])) {
+        $message = "กรุณากรอกชื่อผู้ใช้และรหัสผ่านของท่านด้วย";
+    } else if (empty($_POST['username']) && !empty($_POST['password'])) {
+        $message = "กรุณากรอกชื่อผู้ใช้ของท่านด้วย";
+    } else if (!empty($_POST['username']) && empty($_POST['password'])) {
+        $message = "กรุณากรอกรหัสผ่านของท่านด้วย";
+    }
+}
 ?>
 
 
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-    <title>ระบบลงทะเบียนออนไลน์</title>
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="style.css" />
-    <style type="text/css">
-
-        .style25 {font-size: 11px; font-family: Tahoma; }
-        .style9 {font-size: 12px}
-        .style7 {color: #3987FB; font-size: 14px; }
-        .style26 {
-            font-size: 14px;
-            font-weight: bold;
-        }
-        .style28 {font-size: 12px; font-weight: bold; }
-        .o {
-            color: #000;
-        }
-        oo {
-            font-size: 24px;
-        }
-        .BorderBorder .Border .Columns .MainColumn .ArticleBorder .Article table tr td {
-            color: #060;
-            font-family: Arial, Helvetica, sans-serif;
-            text-align: left;
-        }
-        oo {
-            font-size: 12px;
-        }
-        textarea:focus, input:focus {
-            border: 2px solid #2B7C41;
-        }
-    </style>
-
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
+    <link rel="stylesheet" href="style.css"/>
+    <style type="text/css"></style>
 </head>
+
 <body>
-<div class-="BodyContent">
+<div class="BodyContent">
     <div class="BorderBorder">
-        <div class="BorderBL"><div></div></div>
-        <div class="BorderBR"><div></div></div>
+        <div class="BorderBR">
+            <div></div>
+        </div>
         <div class="BorderTL"></div>
-        <div class="BorderTR"><div></div></div>
-        <div class="BorderR"><div></div></div>
-        <div class="BorderB"><div></div></div>
+        <div class="BorderTR">
+            <div></div>
+        </div>
+        <div class="BorderR">
+            <div></div>
+        </div>
+        <div class="BorderB">
+            <div></div>
+        </div>
         <div class="BorderL"></div>
         <div class="Border">
 
             <div class="Menu">
                 <ul>
-                    <a href="index.php" class="MenuButton"><span>หน้าหลัก</span></a><a href="college.php" class="MenuButton"><span>วิทยาลัย</span></a><a href="course.php" class="MenuButton"><span>หลักสูตร</span></a><a href="ann.php" class="MenuButton"><span>ประชาสัมพันธ์</span> </a><a href="gallary.php" class="MenuButton"><span>ภาพกิจกรรม</span></a><a href="contact_us.php" class="MenuButton"><span>ติดต่อเรา</span></a>
+                    <a href="index.php" class="MenuButton"><span>หน้าหลัก</span></a>
+                    <a href="college.php" class="MenuButton"> <span>วิทยาลัย</span></a>
+                    <a href="course.php" class="MenuButton"><span>หลักสูตร</span></a>
+                    <a href="ann.php" class="MenuButton"><span>ประชาสัมพันธ์</span></a>
+                    <a href="gallary.php" class="MenuButton"><span>ภาพกิจกรรม</span></a>
+                    <a href="contact_us.php" class="MenuButton"><span> ติดต่อเรา</span></a>
                     <input name="text" type="text" style="width:120px"/>
                     <span class="ButtonInput"><span>
-                    <input type="button" value="Search"/>
-                </span></span>
+                 <input type="button" value="Search"/>
+                 </span></span>
                 </ul>
             </div>
-            <div class="Header">
 
+            <div class="Header">
                 <div class="HeaderTitle">
                     <div align="left"><img src="images/banner.jpg" width="836" height="250"></div>
                     <h1>&nbsp;</h1>
                 </div>
             </div>
 
-            <div class="Columns"><div class="Column1">
+            <div class="Columns">
+                <div class="Column1">
 
                     <div class="Block">
 
@@ -81,11 +104,13 @@ if(!isset($_SESSION['username'])) // To check login user if already login then h
 
                         </table>
 
-                        <? if(isset($message)){echo $message;} ?>
-
-                        <!--######################################   To show login form if user do not login ##################################-->
-
-                        <? echo '<form action="" method="post">
+                        <?php if (!empty($message)) {
+                            echo "<span style=\"color:red\">$message</span>";
+                        }
+                        ?>
+                        <?php if (!isset($_SESSION['username']) || !isset($_SESSION['username'])) {
+                            //  To show login form if user do not login
+                            echo '<form action="" method="post">
 		<table width="150" border="0" align="left" cellpadding="0" cellspacing="0">
               <tr>
                 <td></td>
@@ -136,86 +161,47 @@ if(!isset($_SESSION['username'])) // To check login user if already login then h
             </p>
             <p>&nbsp;</p>
         <table width="150" border="0" cellpadding="0" cellspacing="0">
-                  <tr>
-                    <td><div align="center"><a href="register.php"><img src="images/register.gif"  width="130" height="35"></a></div></td>
-                  </tr>
-                </table>
-				';
-                        }
-                        else
-                        {
-#########################   IF user already logined display wellcome below  #############################
-                            echo '
-		<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <tr>
+            <td><div align="center"><a href="register.php"><img src="images/register.gif"  width="130" height="35"></a></div></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </table>
+                
+                
+			            <?php '; ?>
 
-  
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="style.css" />
-    <style type="text/css">
-<!--
-.style25 {font-size: 11px; font-family: Tahoma; }
-.style9 {font-size: 12px}
-.style7 {color: #3987FB; font-size: 14px; }
-.style26 {
-	font-size: 14px;
-	font-weight: bold;
-}
-.style28 {font-size: 12px; font-weight: bold; }
--->
-    </style>
-</head>
-<body>
-    <div class="BodyContent">
-<div class="BorderBorder"><div class="BorderBL"><div></div></div><div class="BorderBR"><div></div></div><div class="BorderTL"></div><div class="BorderTR"><div></div></div>
-      <div class="BorderR"><div></div></div><div class="BorderB"><div></div></div><div class="BorderL"></div>
-      <div class="Border">
+                        <?php } else { ?>
 
-        <div class="Menu">
-            <ul>
-              <li></li> 
-              <li></li> 
-              <li></li> <li></li> 
-              <a href="index.php" class="MenuButton"><span>หน้าหลัก</span></a><a href="college.php" class="MenuButton">  <span>วิทยาลัย</span></a><a href="course.php" class="MenuButton"><span>หลักสูตร</span></a><a href="ann.php" class="MenuButton"><span>ประชาสัมพันธ์</span> </a><a href="gallary.php" class="MenuButton"><span>ภาพกิจกรรม</span></a><a href="contact_us.php" class="MenuButton"><span> ติดต่อเรา</span></a>
-                 <input name="text" type="text" style="width:120px" />
-                 <span class="ButtonInput"><span>
-                 <input type="button" value="Search" />
-                 </span></span></ul>
-        </div>
-        <div class="Header">
-        <div class="HeaderTitle">
-          <div align="left"><img src="images/banner.jpg" width="836" height="250"></div>
-          <h1>&nbsp;</h1>
-        </div>
-        </div><div class="Columns"><div class="Column1">
-         
-          <div class="Block">
-            
-            <span class="BlockHeader"><span>Online Register</span></span>
-            <table width="150" border="0" align="left" cellpadding="0" cellspacing="0">
-             
-            </table>
+                            <?php //  remove the HTML code of login ?>
 
-
-';
+                            <?php // If user already logined, so display wellcome section below ?>
+                            <?php echo ' '; ?>
+                            <?php
                             echo '<br><span class="style7">ยินดีต้อนรับ ::</span>';
-                            echo '<span class="style26 "> '.$username.' </span><br>';
-                            echo '<span class="style7"><a href="std_profile.php" style="color: #3987FB; text-decoration: none">ข้อมูลส่วนตัว</a></span><br>';
-                            echo '<span class="style7"><a href="logout.php" style="color: #3987FB; text-decoration: none">ออกจากระบบ</a></span ><br>';
+                            echo '<span class="style26 "> ' . $username . ' </span><br>';
+                            echo '<span class="style7"><a href="std_profile.php">ข้อมูลส่วนตัว</a></span><br>';
+                            echo '<span class="style7"><a href="logout.php">ออกจากระบบ</a><span class="style7"><br>';
                         }
                         ?>
                         <br>
                     </div>
                     <div class="Block">
-
                         <span class="BlockHeader"><span>Menu</span></span>
                         <div class="BlockContentBorder">
-
                             <ul>
-                                <li><span class="style7"><a href="index.php" style="color: #3987FB; text-decoration: none">หลักสูตรที่เปิด</a></span></li>
-                                <li><span class="style7"><a href="manual.pdf" style="color: #3987FB; text-decoration: none">คู่มือการลงทะเบียน</a></span></li>
+                                <li><span class="style7"><a href="index.php"
+                                                            style="color: #3987FB; text-decoration: none">หลักสูตรที่เปิด</a></span>
+                                </li>
+                                <li><span class="style7"><a href="manual.pdf"
+                                                            style="color: #3987FB; text-decoration: none">คู่มือการลงทะเบียน</a></span>
+                                </li>
                             </ul>
                         </div>
                     </div>
+                </div>
+</body>
+</html>
+
