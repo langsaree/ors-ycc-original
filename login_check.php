e05bbc0b923ce87c392c6a1a00048c74ab156d3d
hassan-z-hub <sano0o.saiburi@gmail.com>
2019-08-20 15:46:06 +0700

    edit Index

--- login_check.php
+++ login_check.php
@@ -72,15 +72,3 @@ if(!isset($_SESSION['logined'])) {
    
 }
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
-<meta http-equiv="refresh" content="1; url=index.php">
-<title>login check</title>
-</head>
-
-<body>
-</body>
-</html>
