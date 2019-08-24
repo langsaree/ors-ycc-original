9cc95d094360566740a33c15ee385b8676f3c85c
HossniSama <34706438+HossniSama@users.noreply.github.com>
2019-08-25 04:42:30 +0700

    Update contact_us.php

--- contact_us.php
+++ contact_us.php
@@ -109,32 +109,7 @@ include('login_check.php');
 		else
 		{
 		echo '
-		<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-    <title>ระบบลงทะเบียนออนไลน์</title>
-    <link rel="stylesheet" href="style.css" />
-    <style type="text/css">
-<!--
-.style25 {font-size: 11px; font-family: Tahoma; }
-.style9 {font-size: 12px}
-.style7 {color: #3987FB; font-size: 14px; }
-.style26 {
-	font-size: 14px;
-	font-weight: bold;
-}
-.style30 {
-	color: #3987FB;
-	font-size: 16px;
-	font-weight: bold;
-}
-.style28 {font-size: 12px; font-weight: bold; }
--->
-    </style>
-</head>
-<body>
     <div class="BodyContent">
 <div class="BorderBorder"><div class="BorderBL"><div></div></div><div class="BorderBR"><div></div></div><div class="BorderTL"></div><div class="BorderTR"><div></div></div>
       <div class="BorderR"><div></div></div><div class="BorderB"><div></div></div><div class="BorderL"></div>
@@ -189,7 +164,8 @@ include('login_check.php');
         </div>
 
         </div><div class="MainColumn">
-        <div class="ArticleBorder"><div class="ArticleBL"><div></div></div><div class="ArticleBR"><div></div></div><div class="ArticleTL"></div><div class="ArticleTR"><div></div></div><div class="ArticleT"></div><div class="ArticleR"><div></div></div><div class="ArticleB"><div></div></div><div class="ArticleL"></div>
+        <div class="ArticleBorder">
+            <div class="ArticleBL"><div></div></div><div class="ArticleBR"><div></div></div><div class="ArticleTL"></div><div class="ArticleTR"><div></div></div><div class="ArticleT"></div><div class="ArticleR"><div></div></div><div class="ArticleB"><div></div></div><div class="ArticleL"></div>
  <?
 session_start();
 if($_POST['action']){
