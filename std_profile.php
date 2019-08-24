e95d6c118bf4f98dc41d605e2c6690c5c0fbf950
Raiyan Chayen <raiyan.chayen@gmail.com>
2019-08-24 07:30:18 +0700

    Update std_profile.php
    + edit login form
    - clear style to style.css
    - make code comfort to see
    + include footer.php

--- std_profile.php
+++ std_profile.php
@@ -4,162 +4,135 @@ $username = $_SESSION['username'];
 if(!isset($_SESSION['username'])){header("location:index.php");}
 //end of check session
 include('db.php');
-
 ?>
-
 
 <!DOCTYPE html>
 <html>
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
     <title>ข้อมูลนักศึกษา </title>
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="style.css" />
-    <style type="text/css">
-<!--
-.style25 {font-size: 11px; font-family: Tahoma; }
-.style7 {color: #3987FB; font-size: 14px; }
-.style46 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 13px; }
-.style47 {font-size: 13px}
-.style54 {font-size: 14px}
-.style55 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 14px; }
-.style56 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 14px; color: #CCCCCC; }
-.style58 {color: #333333}
-.style60 {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 13px;
-	color: #333333;
-	font-weight: bold;
-}
-.style61 {font-size: 13px; color: #333333; }
-a:link {
-	text-decoration: none;
-}
-a:visited {
-	text-decoration: none;
-}
-a:hover {
-	text-decoration: none;
-}
-a:active {
-	text-decoration: none;
-}
-.style33 {
-	font-family: Geneva, Arial, Helvetica, sans-serif;
-	font-size: 10px;
-	color: #666666;
-}
-.style62 {font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px; color: #333333; }
-.style64 {
-	font-size: 14px;
-	font-weight: bold;
-	color: #333333;
-}
-.style66 {font-family: Geneva, Arial, Helvetica, sans-serif; font-size: 12px; color: #666666; }
-.style67 {font-size: 12px}
--->
-    </style>
+    <style type="text/css"></style>
 </head>
+
 <body>
-    <div class="BodyContent">
-    <div class="BorderBorder"><div class="BorderBL"><div></div></div><div class="BorderBR"><div></div></div><div class="BorderTL"></div><div class="BorderTR"><div></div></div>
-      <div class="BorderR"><div></div></div><div class="BorderB"><div></div></div><div class="BorderL"></div>
-      <div class="Border">
-
-        <div class="Menu">
-            <ul>
-              <li></li> 
-              <li></li> 
-              <li></li> <li></li> 
-              <a href="index.php" class="MenuButton"><span>หน้าหลัก</span></a><a href="#" class="MenuButton">  <span>วิทยาลัย</span></a><a href="#" class="MenuButton"><span>หลักสูตร</span></a><a href="#" class="MenuButton"><span>ประชาสัมพันธ์</span> </a><a href="#" class="MenuButton"><span>ภาพกิจกรรม</span></a><a href="#" class="MenuButton"><span> ติดต่อเรา</span></a>
-                 <input name="text" type="text" style="width:120px" />
-                 <span class="ButtonInput"><span>
-                 <input type="button" value="Search" />
-                 </span></span></ul>
+<div class="BodyContent">
+    <div class="BorderBorder">
+        <div class="BorderBR">
+            <div></div>
         </div>
-        <div class="Header">
-        <div class="HeaderTitle">
-          <div align="left"><img src="images/banner.jpg" width="836" height="250"></div>
-          <h1>&nbsp;</h1>
+        <div class="BorderTL"></div>
+        <div class="BorderTR">
+            <div></div>
         </div>
+        <div class="BorderR">
+            <div></div>
         </div>
+        <div class="BorderB">
+            <div></div>
+        </div>
+        <div class="BorderL"></div>
+        <div class="Border">
 
-
-
-
-          <div class="Columns">
-              <div class="Column1">
-          <div class="Block">
-          
-            <span class="BlockHeader"><span>Online Register</span></span>
-            <table width="163" border="0" align="left" cellpadding="0" cellspacing="0">
-              <tr>
-                <td width="163">&nbsp;</td>
-              </tr>
-              <tr>
-                <td><span class="style64"><span style="color: #2192CF">ยินดีต้อนรับ</span> ::</span> <? echo  '<span style="font-weight:bold; font-size:13px"> '.$username.' </span>'; ?></td>
-              </tr>
-              <tr>
-                <td>&nbsp;</td>
-              </tr>
-            </table>
-        
-            
-           
-            <br>
-            <br>
-          </div>
-
-          <div class="Block">
-
-            <span class="BlockHeader"><span>Menu</span></span>
-            <div class="BlockContentBorder">
-
+            <div class="Menu">
                 <ul>
-                    <li><span class="style7"><a href="index.php" style="color: #3987FB; text-decoration: none">หลักสูตรที่เปิด</a></span></li>
-                    <li><span class="style7"><a href="manual.pdf" style="color: #3987FB; text-decoration: none">คู่มือการลงทะเบียน</a></span></li>
+                    <a href="index.php" class="MenuButton"><span>หน้าหลัก</span></a>
+                    <a href="college.php" class="MenuButton"> <span>วิทยาลัย</span></a>
+                    <a href="course.php" class="MenuButton"><span>หลักสูตร</span></a>
+                    <a href="ann.php" class="MenuButton"><span>ประชาสัมพันธ์</span></a>
+                    <a href="gallary.php" class="MenuButton"><span>ภาพกิจกรรม</span></a>
+                    <a href="contact_us.php" class="MenuButton"><span> ติดต่อเรา</span></a>
+                    <input name="text" type="text" style="width:120px"/>
+                    <span class="ButtonInput"><span>
+                 <input type="button" value="Search"/>
+                 </span></span>
                 </ul>
-          </div>
-        </div>
+            </div>
 
+            <div class="Header">
+                <div class="HeaderTitle">
+                    <div align="left"><img src="images/banner.jpg" width="836" height="250"></div>
+                    <h1>&nbsp;</h1>
+                </div>
+            </div>
 
+            <div class="Columns">
+              <div class="Column1">
 
+                  <div class="Block">
+          
+                    <span class="BlockHeader"><span>Online Register</span></span>
+                        <table width="163" border="0" align="left" cellpadding="0" cellspacing="0">
+                          <tr>
+                            <td width="163">&nbsp;</td>
+                          </tr>
+                          <tr>
+                            <td><span class="style64"><span style="color: #2192CF">ยินดีต้อนรับ</span> ::</span> <? echo  '<span style="font-weight:bold; font-size:13px"> '.$username.' </span>'; ?></td>
+                          </tr>
+                          <tr>
+                            <td>&nbsp;</td>
+                          </tr>
+                        </table>
+            <br>
+            <br>
+                  </div>
 
+                  <div class="Block">
 
+                      <span class="BlockHeader"><span>Menu</span></span>
+                      <div class="BlockContentBorder">
 
-
-
-
+                          <ul>
+                              <li><span class="style7"><a href="index.php" style="color: #3987FB; text-decoration: none">หลักสูตรที่เปิด</a></span></li>
+                              <li><span class="style7"><a href="manual.pdf" style="color: #3987FB; text-decoration: none">คู่มือการลงทะเบียน</a></span></li>
+                          </ul>
+                      </div>
+                  </div>
               </div>
 
-              <div class="MainColumn">
-        <div class="ArticleBorder"><div class="ArticleBL"><div></div></div><div class="ArticleBR"><div></div></div><div class="ArticleTL"></div><div class="ArticleTR"><div></div></div><div class="ArticleT"></div><div class="ArticleR"><div></div></div><div class="ArticleB"><div></div></div><div class="ArticleL"></div>
-       
-               <div class="Article">
-            <table width="650" height="961" border="0" align="center" cellpadding="0" cellspacing="5">
-          <tr>
-          
+                <div class="MainColumn">
+                    <div class="ArticleBorder">
+                        <div class="ArticleBL">
+                            <div></div>
+                        </div>
+                        <div class="ArticleBR">
+                            <div></div>
+                        </div>
+                        <div class="ArticleTL">
 
+                        </div>
+                        <div class="ArticleTR">
+                            <div></div>
+                        </div>
+                        <div class="ArticleT">
 
-       <td width="659" height="951" align="right" valign="top">
-           <div align="left">
-         <p class="style55"><span class="style58">::: <strong>ข้อมูลนักศึกษา วิทยาลัยชุมชนยะลา</strong> :::</span><br></p>
-           </div>
+                        </div>
+                        <div class="ArticleR">
+                            <div></div>
+                        </div>
+                        <div class="ArticleB">
+                            <div></div>
+                        </div>
+                        <div class="ArticleL">
+
+                        </div>
+
+                    <div class="Article">
+
+                        <table width="650" height="961" border="0" align="center" cellpadding="0" cellspacing="5">
+                          <tr>
+                            <td width="659" height="951" align="right" valign="top">
+                                <div align="left">
+                                    <p class="style55"><span class="style58">::: <strong>ข้อมูลนักศึกษา วิทยาลัยชุมชนยะลา</strong> :::</span><br></p>
+                                </div>
 
 
  <?php
-
  $sql_view = "SELECT * FROM student WHERE username ='$username' ";
  $result_view = mysqli_query($connection, $sql_view);
  $row = mysqli_fetch_array($result_view);
-
-
-//$sql = "SELECT * FROM student WHERE username= '.$username.' ";
-//$result = mysqli_query($connection, $sql);
-////or die("Error: ".mysqli_error($connection));
-//while($row=mysqli_fetch_array($result))
-
 ?>
        
          <table width="637" height="62" border="0" cellpadding="0" cellspacing="5">
@@ -193,10 +166,10 @@ a:active {
        </table>
          <legend></legend>
 
-<div align="center"></div>
+    <div align="center"></div>
 
-<table width="600" border="0" align="center" cellpadding="0" cellspacing="5">
-  <tr>
+                <table width="600" border="0" align="center" cellpadding="0" cellspacing="5">
+                  <tr>
                     <td width="27" height="18">&nbsp;</td>
                     <td width="196" style="text-align: right; font-weight: bold; color: #333;" >ชื่อล็อกอิน :</span></td>
                     <td colspan="4"><?php echo $row['username']; ?>
@@ -400,29 +373,19 @@ a:active {
                     <td>&nbsp;</td>
                     <td>&nbsp;</td>
                   </tr>
-
-
-
-
               </table>
-                <table width="640" border="0" align="center" cellpadding="0" cellspacing="2">
-                  <tr>
-                    <td width="613"><span class="style56" style="text-align: center">&nbsp;&nbsp;----------------------------------------------------------------------------------------</span></td>
-                  </tr>
-                </table>
-        </table>
-       
-          </div>
-        </div>
 
+                 <table width="640" border="0" align="center" cellpadding="0" cellspacing="2">
+                     <tr>
+                         <td width="613"><span class="style56" style="text-align: center">&nbsp;&nbsp;----------------------------------------------------------------------------------------</span></td>
+                     </tr>
+                 </table>
+                        </table>
 
-
-        <div class="ArticleBorder"><div class="ArticleBL"><div></div></div><div class="ArticleBR"><div></div></div><div class="ArticleTL"></div><div class="ArticleTR"><div></div></div><div class="ArticleT"></div><div class="ArticleR"><div></div></div><div class="ArticleB"><div></div></div><div class="ArticleL"></div>
-        </div>
-        </div>
-          </div>
-        <div class="Footer"><span class="style25">&copy; Copyright Electronic Registration of Yala Community College Design by : Bukhoree | Kholed | Ihsan </span></div>                
-    </div>
-</div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+    <?php  include('include/footer.php');?>
 </body>
 </html>
