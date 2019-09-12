be3fb3165df9759afb75fb35f9d7da406480346b
Suppanut10 <52193749+Suppanut10@users.noreply.github.com>
2019-09-12 15:17:30 +0700

    update coll

--- college.php
+++ college.php
@@ -1,43 +1,11 @@
 <?php
 session_start();
-if(!session_is_registered(username))
-	{
-include('db.php');
-$username = "";
-$password = "";
-if(!isset($_SESSION['logined'])) {
-   if(isset($_REQUEST['username'])) {
-   $username = $_REQUEST['username'];
-   $password = $_REQUEST['password'];
-			if(empty($_REQUEST['username']) && empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกชื่อผู้ใช้และรหัสผ่านของท่านด้วย</span>';
-			} else if(empty($_REQUEST['username']) && !empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกชื่อผู้ใช้ของท่านด้วย</span>';
-			} else if(!empty($_REQUEST['username']) && empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกรหัสผ่านของท่านด้วย</span>';
-			} else {
-			       $sql = "select * from student where username='$username' and password='$password'";
-                   $result=mysql_query($sql);
-                   $count=mysql_num_rows($result);
-                  if($count==1)
-                      {
-					  //$_SESSION['logined'] = true;
-					  //$_SESSION['username'] = $_REQUEST['username'];
-					  //$_SESSION['password'] = $_REQUEST['password'];
-					  session_register("username");
-                      session_register("password");
-					  //$_SESSION['username'] = $value["username"];
-                      //$_SESSION['password'] = $value["password"];
-					  header("location:std_profile.php");
-					  }
-				   else
-				   {
-				    $message = '<span style="color:red">ข้อมูลของท่านไม่ถูกต้อง กรุณาตรวจสอบข้อมูลด้วย</span>';
-				   }
-				  }
-			  	}  
-   
-}
+include 'db.php';
+if (!isset($_SESSION["username"])) // To check login user if already login then hide login form
+{
+  include 'login_check.php';
+    ?>
+
 ?>
 
 <!DOCTYPE html>
@@ -49,10 +17,7 @@ if(!isset($_SESSION['logined'])) {
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="style.css" />
     <style type="text/css">
-<!--
-.style25 {font-size: 11px; font-family: Tahoma; }
-.style7 {color: #3987FB; font-size: 14px; }
--->
+
     </style>
 </head>
 <body>
@@ -90,8 +55,12 @@ if(!isset($_SESSION['logined'])) {
 
 
 
-	<?php echo $message; ?>		
-	<? echo	'<form action="" method="post">
+            <?php
+if (isset($message)) {
+        echo $message;
+    }
+    ?>	
+	<?php echo	'<form action="" method="post">
 		<table width="150" border="0" align="left" cellpadding="0" cellspacing="0">
               <tr>
                 <td></td>
@@ -204,11 +173,11 @@ if(!isset($_SESSION['logined'])) {
 
 
 ';
-		echo '<br><span class="style7">ยินดีต้อนรับ ::</span>'; 
-		echo '<span class="style26 "> '.$username.' </span><br>';
-		echo '<span class="style7"><a href="std_profile.php" style="color: #3987FB; text-decoration: none">ข้อมูลส่วนตัว</a></span><br>';
-		echo '<span class="style7"><a href="logout.php" style="color: #3987FB; text-decoration: none">ออกจากระบบ</a></span ><br>';
-		}
+echo '<br><span class="style7">ยินดีต้อนรับ ::</span>';
+echo '<span class="style26 "> ' .$_SESSION["username"]. ' </span><br>';
+echo '<span class="style7"><a href="std_profile.php">ข้อมูลส่วนตัว</a></span><br>';
+echo '<span class="style7"><a href="logout.php">ออกจากระบบ</a><span class="style7"><br>';
+}
 ?>
             <br>
           </div>
