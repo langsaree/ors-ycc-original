145623cfc454158b7295c6f7e12011d740127594
momonosuket <52197288+momonosuket@users.noreply.github.com>
2019-09-16 23:08:40 +0700

    update db
    update db

--- cpanel/db.php
+++ cpanel/db.php
@@ -1,25 +1,20 @@
 <?php
-$db_host="localhost";
-$db_database="ors_ycc";
-//$m="std_register";
-//$n="login";
-$db_username="root";
-$db_password="root";
-//$search = "select * from $m where id ='$id'";
-//$login="SELECT * FROM admin WHERE username='$username' and password='$password'";
-//$login2="SELECT * FROM student WHERE username='$username' and password='$password'";
-//$login3="SELECT * FROM lecture WHERE username='$username' and password='$password'";
-$connection = mysql_connect($db_host, $db_username, $db_password);
-if (!$connection)
-{
-die("Could not connect with db".mysql_error());
+//define connection elements
+$dbHost = "localhost:3306";
+$dbDatabase = "ors_ycc";
+$dbUsername = "root";
+$dbPassword = "";
+
+//connect to Database
+$connection = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbDatabase);
+
+//connection checking
+if ($connection) {
+   //set universal encoding
+   mysqli_query( $connection ,"SET NAMES 'utf8'") or die(mysqli_error());
+   
+} else {
+   
+   die("Could not connect with db" . mysqli_error());
 }
-$db_select = mysql_select_db($db_database);
-mysql_query ("SET NAMES 'utf8'");
-//mysql_query(" SET NAMES 'tis620'; ");
-//mysql_query(" SET character_set_results='tis620'; ");
-if(!$db_select)
-{
-die("Could not select db".mysql_error());
-} 
 ?>
