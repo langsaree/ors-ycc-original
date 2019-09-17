3afac08040c90afce95e7c9723eff24bc60250b8
tayata <anas25237@gmail.com>
2019-09-17 16:57:34 +0700

    course
    edited a errer line

--- course.php
+++ course.php
@@ -1,43 +1,8 @@
 <?php
 session_start();
-if(!session_is_registered(username))
+if(!isset($_SESSION["username"]))
 	{
-     include('db.php');
-     $username = "";
-     $password = "";
-     if(!isset($_SESSION['logined'])) {
-      if(isset($_REQUEST['username'])) {
-        $username = $_REQUEST['username'];
-        $password = $_REQUEST['password'];
-			if(empty($_REQUEST['username']) && empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกชื่อผู้ใช้และรหัสผ่านของท่านด้วย</span>';
-			} else if(empty($_REQUEST['username']) && !empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกชื่อผู้ใช้ของท่านด้วย</span>';
-			} else if(!empty($_REQUEST['username']) && empty($_REQUEST['password'])) {
-				$message = '<span style="color:red">กรุณากรอกรหัสผ่านของท่านด้วย</span>';
-			} else {
-			       $sql = "select * from student where username='$username' and password='$password'";
-                   $result=mysql_query($sql);
-                   $count=mysql_num_rows($result);
-                  if($count==1)
-                      {
-					  //$_SESSION['logined'] = true;
-					  //$_SESSION['username'] = $_REQUEST['username'];
-					  //$_SESSION['password'] = $_REQUEST['password'];
-					  session_register("username");
-                      session_register("password");
-					  //$_SESSION['username'] = $value["username"];
-                      //$_SESSION['password'] = $value["password"];
-					  header("location:std_profile.php");
-					  }
-				   else
-				   {
-				    $message = '<span style="color:red">ข้อมูลของท่านไม่ถูกต้อง กรุณาตรวจสอบข้อมูลด้วย</span>';
-				   }
-				  }
-			  	}  
-   
-}
+    include('login_check.php');
 ?>
 
 <!DOCTYPE html>
@@ -48,7 +13,6 @@ if(!session_is_registered(username))
     <title>หลักสูตรที่เปิดสอน</title>
     <link rel="stylesheet" href="style.css" />
     <style type="text/css">
-<!--
 .style25 {font-size: 11px; font-family: Tahoma; }
 .style9 {font-size: 12px}
 .style7 {color: #3987FB; font-size: 14px; }
@@ -57,7 +21,6 @@ if(!session_is_registered(username))
 	font-weight: bold;
 }
 .style28 {font-size: 12px; font-weight: bold; }
--->
     </style>
 </head>
 <body>
@@ -95,8 +58,12 @@ if(!session_is_registered(username))
 
 
 
-	<?php echo $message; ?>		
-	<? echo	'<form action="" method="post">
+ <?php
+if (isset($message)) {
+        echo $message;
+    }
+    ?>
+	<?php echo	'<form action="" method="post">
 		<table width="150" border="0" align="left" cellpadding="0" cellspacing="0">
               <tr>
                 <td></td>
@@ -160,7 +127,6 @@ if(!session_is_registered(username))
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
     <title>หลักสูตรที่เปิดสอน</title>
     <link rel="stylesheet" href="style.css" />
     <style type="text/css">
@@ -181,7 +147,6 @@ if(!session_is_registered(username))
 <div class="BorderBorder"><div class="BorderBL"><div></div></div><div class="BorderBR"><div></div></div><div class="BorderTL"></div><div class="BorderTR"><div></div></div>
       <div class="BorderR"><div></div></div><div class="BorderB"><div></div></div><div class="BorderL"></div>
       <div class="Border">
-
         <div class="Menu">
             <ul>
               <li></li> 
@@ -206,15 +171,14 @@ if(!session_is_registered(username))
             <table width="150" border="0" align="left" cellpadding="0" cellspacing="0">
              
             </table>
-
-
 ';
 		echo '<br><span class="style7">ยินดีต้อนรับ ::</span>'; 
-		echo '<span class="style26 "> '.$username.' </span><br>';
+		echo '<span class="style26 "> '.$_SESSION["username"]. ' </span><br>';
 		echo '<span class="style7"><a href="std_profile.php" style="color: #3987FB; text-decoration: none">ข้อมูลส่วนตัว</a></span><br>';
 		echo '<span class="style7"><a href="logout.php" style="color: #3987FB; text-decoration: none">ออกจากระบบ</a></span ><br>';
-		}
+    }
 ?>
+
             <br>
           </div>
           <div class="Block">
@@ -245,5 +209,6 @@ if(!session_is_registered(username))
         <div class="Footer"><span class="style25">&copy; Copyright Electronic Registration of Yala Community College Design by : Bukhoree | Kholed | Ihsan </span></div>                
     </div>
 </div>
+
     </body>
 </html>
