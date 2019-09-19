1ffaf72c2e3a8a19440808df370bae1b4fe4b602
anasTD <52197543+anasTD@users.noreply.github.com>
2019-09-20 00:12:51 +0700

    edit
    cut some line and connec database

--- cpanel/grade_view.php
+++ cpanel/grade_view.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>view grade</title>
 <style type="text/css">
-<!--
+
 body {
 	margin-left: 0px;
 	margin-top: 0px;
@@ -29,7 +29,7 @@ body {
 	font-size: 24px;
 }
 .style47 {font-family: Verdana, Arial, Helvetica, sans-serif}
--->
+
 </style>
 </head>
 
@@ -56,12 +56,12 @@ body {
     <td height="392" valign="top"><p></p>
       <div align="center">
         <table width="353" height="76" border="0" cellpadding="0" cellspacing="10">
-          <?
+          <?php
 	include('db_connect.php');
 	$sql = "select * from $db_table2 where name ='$name'";
-	$show = mysql_query($sql);
-	while($shows = mysql_fetch_row($show))
-	{
+	$show = mysqli_query($connection,$sql);
+	while($shows = mysqli_fetch_row($show))
+
 	?>
           <tr>
             <td width="173"><span class="style46">รหัสประจำตัวนักศึกษา :</span></td>
