619dca69794f4e3c4e8356d85f890d1fe2c34f98
anasTD <52197543+anasTD@users.noreply.github.com>
2019-09-20 00:12:17 +0700

    edit
    connect database

--- cpanel/lec_delete.php
+++ cpanel/lec_delete.php
@@ -1,19 +1,19 @@
 <title>delete process</title>
-<?
+<?php
 include ('db.php');
 extract($_GET);
-$id= $id;
-$sql = "delete from lecturer where lec_id='$id'";
+$id= $_GET['id'];
+$sql = "delete from lecture where lec_id='$id'";
 
 // to porform query
-$result=mysql_query($sql);
+$result=mysqli_query($connection,$sql) ;
 
 if (!$result)
   {
-  die('Error: ' . mysql_error());
+  die('Error: ' . mysqli_error($connection));
   }
-  else
+  else{
 	$success= '<span style="color:red"> ระบบได้ลบข้อมูลอาจารยเรียบร้อยแล้วค่ะ </span>';
 	include ('lec_view.php');
-
+  }
 ?>
