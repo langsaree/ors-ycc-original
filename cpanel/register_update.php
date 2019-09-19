0407d92d91add48b19c3a8be6a922310ab40004c
anasTD <52197543+anasTD@users.noreply.github.com>
2019-09-19 18:30:00 +0700

    edit
    fix connection database

--- cpanel/register_update.php
+++ cpanel/register_update.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>update register</title>
 <style type="text/css">
-<!--
+
 body {
 	margin-left: 0px;
 	margin-top: 0px;
@@ -32,7 +32,7 @@ body {
 	font-family: Verdana, Arial, Helvetica, sans-serif;
 }
 .style48 {color: #666666}
--->
+
 </style>
 </head>
 
@@ -63,8 +63,8 @@ body {
           <?
 	include('db_connect.php');
 	$sql = "select * from $db_table2 where name ='$name'";
-	$show = mysql_query($sql);
-	while($shows = mysql_fetch_row($show))
+	$show = mysqlI_query($connection,$sql);
+	while($shows = mysqli_fetch_row($show))
 	{
 	?>
           <tr>
