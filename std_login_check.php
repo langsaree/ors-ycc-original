88c7ee724ab29e5c0307d0f137cd8e2bdf742c33
momonosuket <52197288+momonosuket@users.noreply.github.com>
2019-09-19 09:45:03 +0700

    edit
    some line

--- std_login_check.php
+++ std_login_check.php
@@ -10,18 +10,18 @@ $password=$_POST['password2'];
 // To protect MySQL injection (more detail about MySQL injection)
 $username = stripslashes($username);
 $password = stripslashes($password);
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
+$username = mysqli_real_escape_string($username);
+$password = mysqli_real_escape_string($password);
 //$code = stripslashes($code);
 //$code = mysql_real_escape_string($code);
 
 //$sql="SELECT * FROM $n WHERE user='$username' and pass='$password'";
 //$result=mysql_query($sql);
 $sql="SELECT * FROM student WHERE username='$username' and password='$password'";
-$result=mysql_query($sql);
+$result=mysqli_query($sql);
 
 // Mysql_num_row is counting table row
-$count=mysql_num_rows($result);
+$count=mysqli_num_rows($result);
 // If result matched $myusername and $mypassword, table row must be 1 row
 
 if($count==1/*&&strcmp($code,$code_hidden)==0*/)
@@ -45,7 +45,7 @@ ob_end_flush();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+<!-- <meta http-equiv="Content-Type" content="text/html; charset=utf8" /> -->
 <meta http-equiv="refresh" content="1; url=register.php">
 <title>student login check</title>
 </head>
