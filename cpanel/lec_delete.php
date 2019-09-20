4020be974636f787bc59662953df5fbbce0cd19d
Raiyan Chayen <raiyan.chayen@gmail.com>
2019-09-20 10:28:26 +0700

    Update lec_delete.php
    +edit syntax error

--- cpanel/lec_delete.php
+++ cpanel/lec_delete.php
@@ -3,14 +3,14 @@
 include ('db.php');
 extract($_GET);
 $id= $id;
-$sql = "delete from lecturer where lec_id='$id'";
+$sql = "delete from lecture where lec_id='$id'";
 
 // to porform query
-$result=mysql_query($sql);
+$result=mysqli_query($connection,$sql);
 
 if (!$result)
   {
-  die('Error: ' . mysql_error());
+  die('Error: ' . mysqli_error($connection));
   }
   else
 	$success= '<span style="color:red"> ระบบได้ลบข้อมูลอาจารยเรียบร้อยแล้วค่ะ </span>';
