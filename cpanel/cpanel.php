5290831a873a2b72423a1ce5f402e381943190e4
anasTD <52197543+anasTD@users.noreply.github.com>
2019-09-22 00:03:14 +0700

    edit cpanel
    fix eror when log in will next page

--- cpanel/cpanel.php
+++ cpanel/cpanel.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-// if(!session_is_registered(user_admin)){header("location:index.php");}
+if(!$_SESSION['user_admin']){header("location:index.php");}
 //end of check session
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
