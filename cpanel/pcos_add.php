5f0877e28223e9209df9866eb81755c38361bf11
anasTD <52197543+anasTD@users.noreply.github.com>
2019-09-23 22:30:02 +0700

    fix pcos add
    remore comment, connect DB

--- cpanel/pcos_add.php
+++ cpanel/pcos_add.php
@@ -1,5 +1,5 @@
 <?php
-/*include ('db.php');
+include ('db.php');
 $cos_id = $_POST['cos_id'];
 $cos_name = $_POST['cos_name'];
 $cos_period = $_POST['cos_period'];
@@ -8,15 +8,15 @@ $cos_after =$_POST['cos_after'];
 $cos_time =$_POST['cos_time'];
 $date = $cos_day."/". $cos_after ."/". $cos_time;
 //echo $date;
-$cos_lec =$_POST['lec_id'];
+$lec_id =$_POST['lec_id'];
 $cos_comment =$_POST['cos_comment'];
-mysql_query ("SET NAMES 'utf8'");
+// mysqli_query ("SET NAMES 'utf8'");
 $sql = "insert into course(cos_id,cos_name,cos_period,cos_day,lec_id,cos_comment) value('$cos_id','$cos_name','$cos_period','$date','$lec_id','$cos_comment')";
-$result = mysql_query($sql);
+$result = mysqli_query($connection,$sql);
 if (!$result)	
-{ die("Error db".mysql_error()); }
+{ die("Error db".mysqli_error($connection)); }
 else
-{*/
+{
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -24,14 +24,14 @@ else
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
 <style type="text/css">
-<!--
+
 body {
 	margin-left: 0px;
 	margin-top: 0px;
 	margin-right: 0px;
 	margin-bottom: 0px;
 }
--->
+
 </style></head>
 
 <body>
@@ -55,6 +55,6 @@ body {
     <tr>
       <td valign="baseline"><img src="image/footer.jpg" width="1260" height="76" /></td>
     </tr>
-  </table> <? //} ?>
+  </table> <?php } ?>
 </body>
 </html>
