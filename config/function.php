d3ae14049bb8b296e24f13c4c1a2cd08a7bc1c4b
langsaree <langsaree@gmail.com>
2019-09-24 15:01:36 +0700

    refactor directory structure to best practice standard
    + refactor directory structure to best practice standard

---
+++ config/function.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * LangsariSoftware - A Delicious PHP Component
+ *
+ * @package  LangsariEngine
+ * @author   Kholed Langsari <langsaree@gmail.com>
+ */
+ 
+
+function name($getFirstName, $getLastName, $getPhoneNumber)
+    {
+        $firstName = $getFirstName;
+        $lastName  = $getLastName;
+        $getPhoneNumber = $getPhoneNumber;
+        echo "My name is" . $firstName . $lastName . "My phone number is" . $getPhoneNumber;
+    }
+name("Kholed", "Langsari", 12345);
+
+function database() {
+
+}
+
+function login() {
+
+}
+
+function logout () {
+
+}
