260a249472da4c254fbc98e26d85c0cbee507066
anashs <akurock32@gmail.com>
2019-09-24 22:37:03 +0700

    add template

---
+++ template/logined.php
@@ -0,0 +1,4 @@
+<br><span class="style7">ยินดีต้อนรับ ::</span>;
+<span class="style26 "> <?php $_SESSION["username"]?> </span><br>;
+<span class="style7"><a href="std_profile.php">ข้อมูลส่วนตัว</a></span><br>;
+<span class="style7"><a href="logout.php">ออกจากระบบ</a><span class="style7"><br>;
