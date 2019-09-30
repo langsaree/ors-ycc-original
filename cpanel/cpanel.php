0b48b1dbef7257a4c5e3b9acea313dfcc29ff845
Harisyoonuh14 <Harisyoonuh14@gmail.com>
2019-09-30 16:56:43 +0700

    Update cpanel.php

--- cpanel/cpanel.php
+++ cpanel/cpanel.php
@@ -4,8 +4,9 @@ if(!isset($_SESSION["user_admin"])){header("location:index.php");}
 // end of check session
 $user_admin = $_SESSION["user_admin"];
  ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+ 
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Cpanel</title>
