76e59d248ad5869cec595cc8023d4b08918b9c31
anasTD <52197543+anasTD@users.noreply.github.com>
2019-09-30 14:56:44 +0700

    Update cos_add.php
    conect DB

--- cpanel/cos_add.php
+++ cpanel/cos_add.php
@@ -1,5 +1,5 @@
 <?php 
-include ('db.php');
+include ('../db.php');
 session_start();
 //>--code insert using for process add data
 
@@ -125,7 +125,7 @@ body {
       <td height="19" colspan="3"><span class="style4">__________________________________________________________________________________________</span></td>
     </tr>
   </table>
-  <form action="" method="POST">
+  <form action="pcos_add.php" method="POST">
   <table width="652" height="286" border="0" cellpadding="0" cellspacing="0">
     <tr>
       <td bgcolor="#FFFFFF"><table width="652" height="286" border="0" cellpadding="0" cellspacing="0">
