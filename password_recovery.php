e316b3633d16bdfe63f68ca2e91eb6b6cf9ec240
Harisyoonuh14 <Harisyoonuh14@gmail.com>
2019-09-30 13:25:09 +0700

    add button to go contect page

--- password_recovery.php
+++ password_recovery.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Password Recovery</title>
 <p> กรุณาติตต่อ ผู้ดูแลระบบครับ </p>
 </head>
-
-<body>
-</body>
-</html>
+<form action="contact_us.php" method="post" name="form1">
+<input type="submit" name="Submit" value="Submit">
+</form>
