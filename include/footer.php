56d2e077c599fea002ebcfa0ead57e9dadfad3e1
Harisyoonuh14 <Harisyoonuh14@gmail.com>
2019-09-30 13:24:11 +0700

    add footer code

---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="style.css" />
+    <div class="Footer">
+        <span class="style25">&copy; Copyright Electronic Registration of Yala Community College Design by : Bukhoree | Kholed | Ihsan </span>
+    </div>
+
