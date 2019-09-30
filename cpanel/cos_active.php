064d627a2f9328596aa513c65f2c71fcb8803bc9
anasTD <52197543+anasTD@users.noreply.github.com>
2019-09-30 14:56:07 +0700

    Update cos_active.php
    connec DB

--- cpanel/cos_active.php
+++ cpanel/cos_active.php
@@ -1,7 +1,7 @@
 <?php
 //session_start();
 //include('auth.php');
-include('db.php');
+include('../db.php');
 extract ($_GET);
 //$active = $active;
 //$non_active = $non_active;
@@ -124,12 +124,15 @@ non {
     <td>&nbsp;</td>
   </tr>
 </table>
-<table width="1024" border="0" cellpadding="0" cellspacing="0"> <?php if($msg){?>
+<table width="1024" border="0" cellpadding="0" cellspacing="0"> 
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+if($msg){?>
   <tr>
     <td width="26" height="61">&nbsp;</td>
     <td width="465"><p class="one"><br />
       &nbsp;&nbsp;
-      <?php echo $msg ?>
+      <?php echo  $msg ?>
       <br />
       <br />
     </p></td>
