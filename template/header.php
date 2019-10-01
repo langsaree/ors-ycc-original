9274ca9c792f8ec7306e9a9ddb4908741e7beeee
anashs <akurock32@gmail.com>
2019-10-01 14:35:13 +0700

    header

--- template/header.php
+++ template/header.php
@@ -2,53 +2,37 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>ระบบลงทะเบียนออนไลน์</title>
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="style.css" />
 </head>
 <body>
     <div class="BodyContent">
-      <div class="BorderBorder">
-        <!-- <div class="ActiveMenuButtonInput">
-          <div>gyiuhihbuhiubhkbhkbij</div>
-        </div> -->
-        <div class="BorderBR">
-          <div></div>
-        </div>
-        <div class="BorderTL"></div>
-        <div class="BorderTR">
-          <div></div>
-        </div>
-        <div class="BorderR">
-          <div></div>
-        </div>
-        <div class="BorderB">
-          <div></div>
-        </div>
-        <div class="BorderL"></div>
-        <div class="Border">
-          <div class="Menu">
+<div class="BorderBorder"><div class="BorderBL"><div></div></div><div class="BorderBR"><div></div></div><div class="BorderTL"></div><div class="BorderTR"><div></div></div>
+      <div class="BorderR"><div></div></div><div class="BorderB"><div></div></div><div class="BorderL"></div>
+      <div class="Border">
+
+        <div class="Menu">
             <ul>
-              <a href="index.php" class="MenuButton"><span>หน้าหลัก</span></a><a href="college.php" class="MenuButton">
-                <span>วิทยาลัย</span></a><a href="course.php" class="MenuButton"><span>หลักสูตร</span></a><a
-                href="ann.php" class="MenuButton"><span>ประชาสัมพันธ์</span> </a><a href="gallary.php"
-                class="MenuButton"><span>ภาพกิจกรรม</span></a><a href="contact_us.php" class="MenuButton"><span>
-                  ติดต่อเรา</span></a>
-              <input name="text" type="text" style="width:120px" />
-              <span class="ButtonInput"><span>
-                  <input type="button" value="Search" />
-                </span></span>
-            </ul>
-          </div>
-          <div class="Header">
-            <div class="HeaderTitle">
-              <div align="left"><img src="images/banner.jpg" width="836" height="250"></div>
-              <h1>&nbsp;</h1>
-            </div>
-          </div>
-          <div class="Columns">
-            <div class="Column1">
-              <div class="Block">
-                <span class="BlockHeader"><span>Online Register</span></span>
-                <table width="150" border="0" align="left" cellpadding="0" cellspacing="0">
-                </table>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <a href="index.php" class="MenuButton"><span>หน้าหลัก</span></a><a href="college.php" class="MenuButton">  <span>วิทยาลัย</span></a><a href="course.php" class="MenuButton"><span>หลักสูตร</span></a><a href="ann.php" class="MenuButton"><span>ประชาสัมพันธ์</span> </a><a href="gallary.php" class="MenuButton"><span>ภาพกิจกรรม</span></a><a href="contact_us.php" class="MenuButton"><span> ติดต่อเรา</span></a>
+                 <input name="text" type="text" style="width:120px" />
+                 <span class="ButtonInput"><span>
+                 <input type="button" value="Search" />
+                 </span></span></ul>
+        </div>
+        <div class="Header">
+        <div class="HeaderTitle">
+          <div align="left"><img src="images/banner.jpg" width="836" height="250"></div>
+          <h1>&nbsp;</h1>
+        </div>
+        </div><div class="Columns"><div class="Column1">
+          <div class="Block">
+
+            <span class="BlockHeader"><span>Online Register</span></span>
+            <table width="150" border="0" align="left" cellpadding="0" cellspacing="0">
+
+            </table>
