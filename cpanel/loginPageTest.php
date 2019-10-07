2bfe5301d35503dbde5057130a1571aaec6b6477
Harisyoonuh14 <Harisyoonuh14@gmail.com>
2019-10-07 10:48:25 +0700

    Create loginPageTest.php

---
+++ cpanel/loginPageTest.php
@@ -0,0 +1,64 @@
+<FORM AUTOCOMPLETE=OFF  METHOD=POST ACTION=validate.asp>
+<DIV ALIGN=CENTER>
+<BR>
+<BR>
+</div>
+    <div ALIGN=CENTER>
+        <table class="dxrpControl_Aqua" cellspacing="0" cellpadding="0" id="ASPxRoundPanel1" border="0" style="width:400px;border-collapse:collapse;border-collapse:separate;">
+	<tr>
+		<td valign="top"><table cellspacing="0" cellpadding="0" border="0" style="width:400px;border-collapse:collapse;">
+			<tr>
+				<td style="width:7px;height:7px;"><img src="images/App_Themes/Aqua/Web/rpTopLeft.png" alt="" style="height:7px;width:7px;border-width:0px;" /></td>
+				<td style="background-color:#E0EDFF;background-image:url(images/App_Themes/Aqua/Web/rpTopEdge.gif);background-repeat:repeat-x;background-position:left Top;border-top-color:#AECAF0;border-top-style:Solid;border-top-width:1px;">
+				<div style="height:1px;width:1px;overflow:hidden;"> 
+				</div></td><td style="width:7px;height:7px;"><img src="images/App_Themes/Aqua/Web/rpTopRight.png" alt="" style="height:7px;width:7px;border-width:0px;" /></td>
+			</tr><tr>
+				<td style="background-color:#E0EDFF;border-left:1px solid #aecaf0 ;background-image:url(images/App_Themes/Aqua/Web/rpHeaderBackground.gif);background-repeat:repeat-x;background-position:left Top;border-bottom:1px solid #aecaf0 ;"><div style="height:1px;width:1px;overflow:hidden;">
+ 
+				</div></td><td class="dxrpControl_Aqua dxrpHeader_Aqua" style="background-color:#E0EDFF;height:1%;white-space:nowrap;padding-top:1px;vertical-align:Top;background-repeat:repeat-x;border-bottom-width:1px;padding-right:6px;border-bottom-color:#AECAF0;padding-bottom:5px;padding-left:6px;background-image:url(images/App_Themes/Aqua/Web/rpHeaderBackground.gif);border-bottom-style:Solid;background-position:left Top;"><table cellspacing="0" cellpadding="0" border="0" style="width:100%;border-collapse:collapse;border-collapse:separate;">
+					<tr>
+						<td class="dxrp" style="width:100%;vertical-align:Top;white-space:nowrap;"><span id="ASPxRoundPanel1_RPHT"><B>กรุณาป้อนรหัสประจำตัวและรหัสผ่าน</B></span></td>
+					</tr>
+				</table></td><td style="background-color:#E0EDFF;border-right:1px solid #aecaf0 ;background-image:url(images/App_Themes/Aqua/Web/rpHeaderBackground.gif);background-repeat:repeat-x;background-position:left Top;border-bottom:1px solid #aecaf0 ;"><div style="height:1px;width:1px;overflow:hidden;">
+ 
+				</div></td>
+			</tr><tr>
+				<td style="background-color:White;background-image:url(images/App_Themes/Aqua/Web/rpLeftEdge.gif);background-repeat:repeat-y;background-position:left Top;border-left-color:#AECAF0;border-left-style:Solid;border-left-width:1px;"><div style="height:1px;width:1px;overflow:hidden;">
+ 
+				</div></td><td id="ASPxRoundPanel1_RPC" class="dxrp" style="background-color:White;width:100%;vertical-align:Top;padding-left:14px;padding-right:14px;padding-top:14px;padding-bottom:14px;">
+
+				<TABLE BORDER=0 CELLPADDING=0 CELLSPACING=3>
+					<TR><TD ROWSPAN=3>&nbsp;</TD>
+						<TD ROWSPAN=3 VALIGN=TOP><IMG SRC=images/common/misc/key.gif WIDTH=60 HEIGHT=100></TD>
+						<TD ROWSPAN=3 WIDTH=10></TD>
+						<td class="dxrp" style="width:80;vertical-align:CENTER;white-space:nowrap;">
+							Username</td>
+						<TD>
+							<INPUT style='width:160;' TYPE=TEXT NAME=f_uid SIZE=15 VALUE="">
+						</TD>
+					</TR>
+					<TR>
+						<td class="dxrp" style="width:80;vertical-align:CENTER;white-space:nowrap;"><span id="ASPxRoundPanel1_RPHT">Password</span></td>
+						<TD><INPUT TYPE=PASSWORD style="width:160;" NAME=f_pwd SIZE=15 ><INPUT  TYPE=HIDDEN NAME=BUILDKEY value=96729 ></TD>
+					</TR>
+					<TR>
+						<TD></TD>
+						<TD>
+							<FONT FACE="Tahoma" ><INPUT TYPE=SUBMIT style="width:100;" VALUE=&nbsp;เข้าสู่ระบบ&nbsp></FONT>
+						</TD>
+					</TR>
+				</TABLE>
+
+				</td><td style="background-color:White;background-image:url(images/App_Themes/Aqua/Web/rpRightEdge.gif);background-repeat:repeat-y;background-position:left Top;border-right-color:#AECAF0;border-right-style:Solid;border-right-width:1px;"><div style="height:1px;width:1px;overflow:hidden;">
+ 
+				</div></td>
+			</tr><tr>
+				<td style="width:7px;height:7px;"><img src="images/App_Themes/Aqua/Web/rpBottomLeft.png" alt="" style="height:7px;width:7px;border-width:0px;" /></td><td style="background-color:White;background-image:url(images/App_Themes/Aqua/Web/rpBottomEdge.gif);background-repeat:repeat-x;background-position:left Bottom;border-bottom-color:#AECAF0;border-bottom-style:Solid;border-bottom-width:1px;"><div style="height:1px;width:1px;overflow:hidden;">
+ 
+				</div></td><td style="width:7px;height:7px;"><img src="images/App_Themes/Aqua/Web/rpBottomRight.png" alt="" style="height:7px;width:7px;border-width:0px;" /></td>
+			</tr>
+		</table><BR></td>
+	</tr>
+	<tr><td class="dxrp" align ="CENTER" style="width:100%;white-space:nowrap;"><FONT SIZE=2 COLOR=#FF0000></FONT></TD></tr>
+</table>
+    </div>
