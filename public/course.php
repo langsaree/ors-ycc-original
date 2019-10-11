f0ee0ff0078b947e1c803db02fcff9a08e69920b
Harisyoonuh14 <Harisyoonuh14@gmail.com>
2019-10-11 14:20:47 +0700

    refactor directory structure to best practice standard

---
+++ public/course.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+include('include/login_check.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>หลักสูตรที่เปิดสอน</title>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="style.css" />
+    <style type="text/css"></style>
+</head>
+
+<body>
+<div class="MainColumn">
+    <div class="ArticleBorder">
+        <div class="ArticleBL">
+            <div></div>
+        </div>
+        <div class="ArticleBR">
+            <div></div>
+        </div>
+        <div class="ArticleTL">
+
+        </div>
+        <div class="ArticleTR">
+            <div></div>
+        </div>
+        <div class="ArticleT">
+
+        </div>
+        <div class="ArticleR">
+            <div></div>
+        </div>
+        <div class="ArticleB">
+            <div></div>
+        </div>
+        <div class="ArticleL">
+        </div>
+
+        <div class="Article">
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+              </tr>
+            <p align="center">ยังไม่มีหลักสูตรที่เปิดสอน</p>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+              </tr>
+        </div>
+    </div>
+</div>
+</div>
+<?php  include('include/footer.php');?>
+</body>
+</html>
