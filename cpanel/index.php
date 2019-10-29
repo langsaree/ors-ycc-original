063870d15bc077de1e9486eed38eb676ce56824b
Ateef10 <52196894+Ateef10@users.noreply.github.com>
2019-10-29 20:53:34 +0700

    make php 7

--- cpanel/index.php
+++ cpanel/index.php
@@ -1,10 +1,22 @@
 <?php
-//session_start();
-//if(session_is_registered("user_admin")){header("location:cpanel.php");}
-//ob_start(); 
-include("db.php");
-$username=$_POST["username"];
-$password=$_POST["password"];
+session_start();
+
+if(isset($_SESSION["user_admin"])){header("location:cpanel.php");}
+ob_start(); 
+include("../db.php");
+
+if(empty($username) && empty($password)){
+
+}
+    
+$username= isset($_POST['username']) ? $_POST['username'] : "";
+$password= isset($_POST['password']) ? $_POST['password'] : "";
+$sql = "select * from admin where username='$username' and password='$password'";
+$result=mysqli_query($connection, $sql);
+$count=mysqli_num_rows($result);
+
+//$username=$_POST["username"];
+//$password=$_POST["password"];
 //$code=$_POST['code'];
 //$code_hidden=$_POST['code_hidden'];
 
@@ -19,20 +31,16 @@ $password=$_POST["password"];
 //$sql="SELECT * FROM $n WHERE user='$username' and pass='$password'";
 //$result=mysql_query($sql);
 //$sql="SELECT * FROM $n WHERE user='$username' and pass='$password'";
-$sql = "select * from admin where username='$username' and password='$password'";
-$result=mysql_query($sql);
-$count=mysql_num_rows($result);
+
 if($count==1/*&&strcmp($code,$code_hidden)==0*/)
 {
-$user_admin = $username;
-$pass_admin = $password;
-session_register("user_admin");
-session_register("pass_admin");
-header("location:cpanel.php");
+    $_SESSION["user_admin"] = $username;
+    header("location:cpanel.php");
+    
 }
 else
  {
-$error='<span style="color:red">ชื่ิอเข้าระบบและรหัสผ่านผิดค่ะ กรุณาลองใหม่</span>';
+$message = '<span style="color:red">ชื่ิอเข้าระบบและรหัสผ่านผิดค่ะ กรุณาลองใหม่</span>';
 //echo "Wrong username and password";
 } 
 //ob_end_flush();
@@ -50,7 +58,7 @@ $error='<span style="color:red">ชื่ิอเข้าระบบและ
 <script type='text/javascript' src='js/jquery.min.js'></script>
 <script type='text/javascript' src='js/example.js'></script>
     <style type="text/css">
-<!--
+
 .style2 {color: #0066FF}
 .style3 {color: #666666}
 .style5 {
@@ -61,7 +69,7 @@ $error='<span style="color:red">ชื่ิอเข้าระบบและ
 	font-weight: bold;
 	text-decoration:none;
 }
--->
+
     </style>
 </head>
 
