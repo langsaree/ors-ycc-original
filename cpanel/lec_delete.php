ad0dcf1ff09a288ca1c33823fef1e9b414737e03
musron77 <52195587+musron77@users.noreply.github.com>
2019-10-29 13:04:59 +0700

    edit
    put echo to show row
    connect db
    open style

--- cpanel/lec_delete.php
+++ cpanel/lec_delete.php
@@ -1,16 +1,16 @@
 <title>delete process</title>
-<?
-include ('db.php');
+<?php
+include ('../db.php');
 extract($_GET);
 $id= $id;
 $sql = "delete from lecturer where lec_id='$id'";
 
 // to porform query
-$result=mysql_query($sql);
+$result=mysqli_query($connection, $sql);
 
 if (!$result)
   {
-  die('Error: ' . mysql_error());
+  die('Error: ' . mysqli_error());
   }
   else
 	$success= '<span style="color:red"> ระบบได้ลบข้อมูลอาจารยเรียบร้อยแล้วค่ะ </span>';
