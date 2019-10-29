3e947ee0507c7a9f7f7fa8b348ab5459f25594bf
Ateef10 <52196894+Ateef10@users.noreply.github.com>
2019-10-29 20:55:04 +0700

    make php 7

--- db.php
+++ db.php
@@ -1,23 +1,11 @@
 <?php
 //define connection elements
-$dbHost = "localhost:8889";
-$dbDatabase = "ors_ycc";
+$dbHost = "localhost";
+$dbDatabase = "orc_ycc";
 $dbUsername = "root";
-$dbPassword = "root";
+$dbPassword = "";
 
 //connect to Database
-$connection = mysql_connect($dbHost, $dbUsername, $dbPassword);
-
-//connection checking
-if ($connection) {
-   $db_select = mysql_select_db($dbDatabase);
-   
-   //set universal encoding
-   mysql_query("SET NAMES 'utf8'") or die(mysql_error());
-   if (!$db_select) {
-      die("Could not select db" . mysql_error());
-   }
-} else {
-   die("Could not connect with db" . mysql_error());
-}
+$connection = mysqli_connect($dbHost, $dbUsername, $dbPassword,$dbDatabase);
+mysqli_query($connection, "SET NAMES 'utf8'") or die(mysqli_error());
 ?>
