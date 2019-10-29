350fa157390d502b860067a7039abb975a1f8999
sabeenadoloh <sabeenadoloh@gmail.com>
2019-10-30 03:01:42 +0700

    update

--- cpanel/db.php
+++ cpanel/db.php
@@ -1,25 +1,31 @@
 <?php
-$db_host="localhost";
-$db_database="ors_ycc";
-//$m="std_register";
-//$n="login";
-$db_username="root";
-$db_password="root";
-//$search = "select * from $m where id ='$id'";
-//$login="SELECT * FROM admin WHERE username='$username' and password='$password'";
-//$login2="SELECT * FROM student WHERE username='$username' and password='$password'";
-//$login3="SELECT * FROM lecture WHERE username='$username' and password='$password'";
-$connection = mysql_connect($db_host, $db_username, $db_password);
-if (!$connection)
+//define connection elements
+$dbHost = "localhost";
+$dbDatabase = "ors_ycc";
+$dbUsername = "root";
+$dbPassword = "";
+
+//connect to Database
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbDatabase);
+$conn->query("set names utf8");
+
+//connection checking
+// if ($conn) {
+//    // $db_select = mysqli_select_db($dbDatabase);
+   
+//    //set universal encoding
+//    mysqli_query("SET NAMES 'utf8'") or die(mysqli_error());
+//    if (!$db_select) {
+//       die("Could not select db: " . mysqli_connect_error());
+//    }
+// } else {
+//    die("Could not connect with db" . mysqli_error());
+// }
+
+
+// Check connection
+if (mysqli_connect_error()) 
 {
-die("Could not connect with db".mysql_error());
+    echo "Database Connect failed :" . mysqli_connect_error();
 }
-$db_select = mysql_select_db($db_database);
-mysql_query ("SET NAMES 'utf8'");
-//mysql_query(" SET NAMES 'tis620'; ");
-//mysql_query(" SET character_set_results='tis620'; ");
-if(!$db_select)
-{
-die("Could not select db".mysql_error());
-} 
 ?>
