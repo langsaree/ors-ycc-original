91c5714c17ac3031afdca0504d0c5643076493ed
sabeenadoloh <sabeenadoloh@gmail.com>
2019-10-30 22:44:54 +0700

    Update

--- backend/cos_del.php
+++ backend/cos_del.php
@@ -1,14 +1,13 @@
 <?php
 include('../db.php');
 extract ($_GET);
-//mysql_query ("SET NAMES 'utf8'");
-$id ="";
+// mysql_query ("SET NAMES 'utf8'");
 $cos_id=$id;  
 echo " หลักสูตรที่คุณเลือก <p>ได้ทำการลบออกจากระบบเรียบร้อยแล้วค่ะ!!!</p>";
 //echo $cos_id;
 $result = mysqli_query($conn,"delete from course where cos_id = $cos_id");
 	if(!$result){
-	die("could not query db".mysqli_error());
+	die("could not query db".mysqli_error($conn));
 	}
 	else {
 		header("location:cos_view.php");
